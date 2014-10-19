4bf4a8f30f1efd136fb905ac12bbce2af05753b8
Nisse Bergman <nisse@potmo.com>
2014-10-19 20:43:44 +0200

    initial commit

---
+++ build.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+
+#/usr/texbin/pdflatex -synctex=1 -interaction=nonstopmode test.tex 
+#/usr/texbin/splitindex test.idx
+/usr/texbin/pdflatex -synctex=1 -interaction=nonstopmode test.tex 
+
